c6f8a93e0c0a3123c1cbfd76e0c77a339cafc3ad
Konrad <witt.konrad@gmail.com>
2025-03-23 14:43:47 +0100

    Removed subject from income class

--- AssetDeclarationsApi/AssetDeclarationsApi/Entities/Income.cs
+++ AssetDeclarationsApi/AssetDeclarationsApi/Entities/Income.cs
@@ -3,7 +3,6 @@
     public class Income
     {
         public int Id { get; set; }
-        public string Subject { get; set; }
         public string Description { get; set; }
         public double YearlyValue { get; set; }
         public int AssetDeclarationId { get; set; }
---
+++ AssetDeclarationsApi/AssetDeclarationsApi/Migrations/20250323134258_RemovedIncomeSubject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetDeclarationsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedIncomeSubject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Incomes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Incomes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
