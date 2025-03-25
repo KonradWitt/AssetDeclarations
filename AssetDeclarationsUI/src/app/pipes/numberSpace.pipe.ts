e81de386825583a44ae247135b7b5f5bc2a6672b
Konrad <witt.konrad@gmail.com>
2025-03-25 17:52:54 +0100

    Improved layout of currencies card and logic of numberSpacePipe

--- AssetDeclarationsUI/src/app/pipes/numberSpace.pipe.ts
+++ AssetDeclarationsUI/src/app/pipes/numberSpace.pipe.ts
@@ -6,6 +6,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NumberSpacePipe implements PipeTransform {
   transform(value: number | undefined): string {
     if (!value) return '';
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+    var result: string = '';
+
+    const strVal = value.toString();
+    const split = strVal.split('.');
+
+    if (split[0].length <= 4) return strVal;
+
+    let processedDigits: number = 0;
+    while (processedDigits < split[0].length) {
+      result = split[0][split[0].length - processedDigits - 1] + result;
+      processedDigits++;
+      if (processedDigits % 3 == 0) result = ' ' + result;
+    }
+
+    if (split.length > 1) {
+      result = result + '.' + split[1];
+    }
+
+    return result;
   }
 }
