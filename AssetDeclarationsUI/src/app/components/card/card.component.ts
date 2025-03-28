d59a2f86b4c84d5d8ed9aec3d5cbb7c786fbca7c
Konrad <witt.konrad@gmail.com>
2025-03-28 17:57:20 +0100

    Addedd card icons

--- AssetDeclarationsUI/src/app/components/card/card.component.ts
+++ AssetDeclarationsUI/src/app/components/card/card.component.ts
@@ -7,14 +7,16 @@ import {
   Input,
 } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'card',
-  imports: [MatDivider, NgIf],
+  imports: [MatDivider, NgIf, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   @Input() showFooter: boolean = false;
-  @Input() title: string = "";
+  @Input() title: string = '';
+  @Input() iconName: string = '';
 }
