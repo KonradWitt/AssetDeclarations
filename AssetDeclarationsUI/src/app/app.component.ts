454791d25bbfa528d38036d83cb4f607bd5be492
Konrad <witt.konrad@gmail.com>
2025-04-03 14:52:33 +0200

    moved ai content warning trigger to app.components.ts

--- AssetDeclarationsUI/src/app/app.component.ts
+++ AssetDeclarationsUI/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { AiContentWarningDialogComponent } from './dialogs/ai-content-warning-dialog/ai-content-warning-dialog.component';
 
 interface ILink {
   path: string;
@@ -21,7 +23,7 @@ interface ILink {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AssetDeclarationsUI';
   links: ILink[] = [
     { path: '', label: 'Home' },
@@ -30,4 +32,13 @@ export class AppComponent {
   ];
 
   activeLink = signal<ILink>(this.links[0]);
+
+  constructor(private dialogService: MatDialog) {}
+
+  ngOnInit(): void {
+    if (sessionStorage.getItem('ai-content-warning-displayed') != 'true') {
+      this.dialogService.open(AiContentWarningDialogComponent);
+      sessionStorage.setItem('ai-content-warning-displayed', 'true');
+    }
+  }
 }
--- AssetDeclarationsUI/src/app/pages/home/home.component.ts
+++ AssetDeclarationsUI/src/app/pages/home/home.component.ts
@@ -7,17 +7,7 @@ import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
 import { HighlightsCarouselComponent } from '../../components/highlights-carousel/highlights-carousel.component';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
-import { MessageDialogComponent } from '../../dialogs/message-dialog/message-dialog.component';
-import { AiContentWarningDialogComponent } from '../../dialogs/ai-content-warning-dialog/ai-content-warning-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-home',
@@ -32,15 +22,11 @@ import { AiContentWarningDialogComponent } from '../../dialogs/ai-content-warnin
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   selectedPerson = signal<Person | undefined>(undefined);
   isLoading = signal<boolean>(false);
 
   constructor(private dialogService: MatDialog, private router: Router) {}
-
-  ngOnInit(): void {
-    this.dialogService.open(AiContentWarningDialogComponent);
-  }
 
   onPersonSelected($event: Person) {
     this.router.navigate(['polityk', $event.link], {
