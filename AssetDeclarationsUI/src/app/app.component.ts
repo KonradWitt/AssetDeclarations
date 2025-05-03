ab621ab16b7d7f1b530bffbc0b9e0716f14ee751
Konrad <witt.konrad@gmail.com>
2025-05-03 20:07:17 +0200

    Fixed redirection after logout

--- AssetDeclarationsUI/src/app/app.component.ts
+++ AssetDeclarationsUI/src/app/app.component.ts
@@ -62,6 +62,6 @@ export class AppComponent implements OnInit {
 
   onLogoutClicked() {
     this.authService.logout();
-    this.router.navigate(['']);
+    this.router.navigate(['login']);
   }
 }
